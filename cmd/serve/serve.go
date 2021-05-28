c6f0605ea9439c512588c530fdeda26ca538a177
Chung Wu <chung@like.co>
2021-05-28 12:14:20 +0800

    Remove ignore-height-difference parameter

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -24,6 +24,11 @@ var Command = &cobra.Command{
 			logger.L.Panicw("Cannot acquire connection from database connection pool", "error", err)
 		}
 		err = db.InitDB(conn)
+		if err != nil {
+			logger.L.Panicw("Cannot initialize database", "error", err)
+		}
+		conn.Release()
+
 		listenAddr, err := cmd.PersistentFlags().GetString("listen-addr")
 		if err != nil {
 			logger.L.Panicw("Cannot get listen address from command line parameters", "error", err)
@@ -31,10 +36,6 @@ var Command = &cobra.Command{
 		lcdEndpoint, err := cmd.PersistentFlags().GetString("lcd-endpoint")
 		if err != nil {
 			logger.L.Panicw("Cannot get lcd endpoint address from command line parameters", "error", err)
-		}
-		ignoreHeightDiff, err := cmd.PersistentFlags().GetBool("ignore-height-difference")
-		if err != nil {
-			logger.L.Panicw("Cannot get ignore-height-difference param from command line parameters", "error", err)
 		}
 		if lcdEndpoint[len(lcdEndpoint)-1] == '/' {
 			lcdEndpoint = lcdEndpoint[:len(lcdEndpoint)-1]
@@ -45,20 +46,6 @@ var Command = &cobra.Command{
 			LcdEndpoint: lcdEndpoint,
 		}
 
-		if !ignoreHeightDiff {
-			const heightDiffLimit = 10000
-			dbHeight, err := db.GetLatestHeight(conn)
-			if err != nil {
-				logger.L.Panicw("Cannot get height from database", "error", err)
-			}
-			blockResult, err := poller.GetBlock(&ctx, 0)
-			lcdHeight := blockResult.Block.Height
-			if lcdHeight-dbHeight > heightDiffLimit {
-				logger.L.Fatalw("height difference larger than limit, please run `import` or add --ignore-height-difference flag", "db_height", dbHeight, "lcd_height", lcdHeight, "limit", heightDiffLimit)
-			}
-		}
-		conn.Release()
-
 		go rest.Run(pool, listenAddr, lcdEndpoint)
 		poller.Run(pool, &ctx)
 	},
@@ -67,5 +54,4 @@ var Command = &cobra.Command{
 func init() {
 	Command.PersistentFlags().String("lcd-endpoint", "http://localhost:1317", "LikeCoin chain lite client RPC endpoint")
 	Command.PersistentFlags().String("listen-addr", "localhost:8997", "HTTP API serving address")
-	Command.PersistentFlags().Bool("ignore-height-difference", false, "start serving and polling without import even if the height lags behind too much")
 }
