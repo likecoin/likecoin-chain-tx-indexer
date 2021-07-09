44277a59cb0fab42988efe8ede03085c219a9db6
Chung Wu <chung@like.co>
2021-07-09 14:58:03 +0800

    Better error handling on poller

--- poller/poller.go
+++ poller/poller.go
@@ -16,12 +16,19 @@ import (
 
 const batchSize = 1000
 
+// TODO: move into config
+const sleepInitial = 5 * time.Second
+const sleepMax = 600 * time.Second
+
 func getResponse(client *http.Client, url string) ([]byte, error) {
 	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("non-200 code returned: %d", resp.StatusCode)
+	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
@@ -83,23 +90,23 @@ func getHeight(pool *pgxpool.Pool) (int64, error) {
 	return lastHeight, nil
 }
 
-func poll(pool *pgxpool.Pool, ctx *CosmosCallContext, lastHeight int64) int64 {
+func poll(pool *pgxpool.Pool, ctx *CosmosCallContext, lastHeight int64) (int64, error) {
 	conn, err := db.AcquireFromPool(pool)
 	if err != nil {
-		logger.L.Panicw("Cannot acquire connection from database connection pool", "error", err)
+		return 0, fmt.Errorf("cannot acquire connection from database connection pool: %w", err)
 	}
 	defer conn.Release()
 	batch := db.NewBatch(conn, batchSize)
 	blockResult, err := GetBlock(ctx, 0)
 	if err != nil {
 		// TODO: retry
-		logger.L.Panicw("Cannot get latest block from lcd", "error", err)
+		return 0, fmt.Errorf("cannot get latest block from lcd: %w", err)
 	}
 	maxHeight := blockResult.Block.Height
 	for height := lastHeight + 1; height <= maxHeight; height++ {
 		blockResult, err := GetBlock(ctx, height)
 		if err != nil {
-			logger.L.Panicw("Cannot get block from lcd", "height", height, "error", err)
+			return 0, fmt.Errorf("cannot get block from lcd, error = %w, height = %d", err, height)
 		}
 		for txIndex, tx := range blockResult.Block.Data.Txs {
 			txHash := cmn.HexBytes(tx.Hash())
@@ -107,19 +114,19 @@ func poll(pool *pgxpool.Pool, ctx *CosmosCallContext, lastHeight int64) int64 {
 			url := fmt.Sprintf("%s/txs/%s", ctx.LcdEndpoint, txHash.String())
 			txResJSON, err := getResponse(ctx.Client, url)
 			if err != nil {
-				logger.L.Panicw("Cannot get tx response from lcd", "txhash", txHash, "height", height, "index", txIndex, "error", err)
+				return 0, fmt.Errorf("cannot get tx response from lcd, error = %w, txhash = %s, height = %d, index = %d", err, txHash.String(), height, txIndex)
 			}
 			err = batch.InsertTx(txResJSON, height, txIndex)
 			if err != nil {
-				logger.L.Panicw("Cannot insert transaction", "txhash", txHash, "height", height, "index", txIndex, "tx_json", txResJSON, "error", err)
+				return 0, fmt.Errorf("cannot insert transaction, error = %w, txhash = %s, height = %d, index = %d, tx_json = %s", err, txHash.String(), height, txIndex, string(txResJSON))
 			}
 		}
 	}
 	err = batch.Flush()
 	if err != nil {
-		logger.L.Panicw("Cannot flush transaction batch", "batch", batch, "error", err)
+		return 0, fmt.Errorf("cannot flush transaction batch, error = %w, batch = %v", err, batch)
 	}
-	return maxHeight
+	return maxHeight, nil
 }
 
 func Run(pool *pgxpool.Pool, ctx *CosmosCallContext) {
@@ -127,9 +134,20 @@ func Run(pool *pgxpool.Pool, ctx *CosmosCallContext) {
 	if err != nil {
 		logger.L.Panicw("Cannot get height from database", "error", err)
 	}
+	toSleep := sleepInitial
 	for {
-		lastHeight = poll(pool, ctx, lastHeight)
-		// TODO: move into config
-		time.Sleep(5 * time.Second)
+		lastHeight, err = poll(pool, ctx, lastHeight)
+		if err == nil {
+			// reset sleep time to normal value
+			toSleep = sleepInitial
+		} else {
+			logger.L.Errorw("cannot poll block", "error", err)
+			// exponential back-off with max cap
+			toSleep = toSleep * 2
+			if toSleep > sleepMax {
+				toSleep = sleepMax
+			}
+		}
+		time.Sleep(toSleep)
 	}
 }
