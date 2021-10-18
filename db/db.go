1da601b930a843b3573b2acb09b5d3010852345c
Chung Wu <chung@like.co>
2021-10-19 00:39:53 +0800

    🚑️ Fix indexing error on row size too long

--- db/db.go
+++ db/db.go
@@ -169,7 +169,12 @@ func getEventStrings(events types.StringEvents) []string {
 	for _, event := range events {
 		for _, attr := range event.Attributes {
 			s := fmt.Sprintf("%s.%s=\"%s\"", event.Type, attr.Key, attr.Value)
-			eventStrings = append(eventStrings, s)
+			if len(s) < 8100 {
+				// Cosmos SDK indeed generate meaninglessly long event strings
+				// (e.g. in IBC client update, hex-encoding the whole header)
+				// These event strings are useless and can't be handled by Postgres GIN index
+				eventStrings = append(eventStrings, s)
+			}
 		}
 	}
 	return eventStrings
