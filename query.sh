63bb608990a4b512508ec8c96ddaa8fd13344b34
wancat <wancat@wancat.cc>
2022-03-05 17:13:44 +0800

    Add multiple terms support

--- query.sh
+++ query.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # Create index to enhance performance
-# create index on txs using gin(jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]'));
-TERM=$1
+# CREATE INDEX txs USING GIN(jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]'));
+
+# Usage:
+# ./query.sh [search term]
+# Example:
+# ./query.sh decentralizehk
+# Multiple terms:
+# ./query.sh 'decentralizehk & likecoin'
+
+
+TERM=$@
 psql mydb <<SQL
 select tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}'
   from txs
-  where jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]') @@ plainto_tsquery('English', '$TERM')
+  where jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]') @@ to_tsquery('English', '$TERM')
+  limit 1000
 SQL
