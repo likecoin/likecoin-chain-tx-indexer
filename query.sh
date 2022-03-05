07ae17055ba7ace3d41152d28ca6f32810aa9420
wancat <wancat@wancat.cc>
2022-03-05 16:42:08 +0800

    Create index to increase performance

--- query.sh
+++ query.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+# Create index to enhance performance
+# create index on txs using gin(jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]'));
 TERM=$1
 psql mydb <<SQL
 select tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}'
   from txs
   where jsonb_to_tsvector('english', tx #> '{"tx", "body", "messages", 0, "record", "contentMetadata"}' , '["string"]') @@ plainto_tsquery('English', '$TERM')
-  limit 10
 SQL
