48fc59b09cbfbc5c07df9e5c5dc57b13540fa68a
wancat <wancat@wancat.cc>
2022-03-20 17:13:31 +0800

    Add request examples

--- examples/iscn_by_id.sh
+++ examples/iscn_by_id.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-curl http://localhost:8997/iscn/id?iscn_id=iscn://likecoin-chain/laa5PLHfQO2eIfiPB2-ZnFLQrmSXOgL-NvoxyBTXHvY/1 | jq
+curl http://localhost:8997/iscn/records/id?iscn_id=iscn://likecoin-chain/laa5PLHfQO2eIfiPB2-ZnFLQrmSXOgL-NvoxyBTXHvY/1 | jq
