960b4b4f8850b35594a0f4e676b47cffd0804000
lancatlin <lancatlin@pm.me>
2022-04-16 11:40:34 +0800

    Add TODO mark

--- db/db.go
+++ db/db.go
@@ -158,6 +158,7 @@ func InitDB(conn *pgxpool.Conn) error {
 	if err != nil {
 		return err
 	}
+	// TODO: add support to not only 0 in the future
 	ctx = context.Background()
 	_, err = conn.Exec(ctx, `CREATE INDEX IF NOT EXISTS idx_record ON txs USING GIN((tx #> '{tx, body, messages, 0, record}') jsonb_path_ops)`)
 	if err != nil {
