c4598db6c6a0f58ba3eb99712721cb7ea9216faf
Justin Lin <wancat@wancat.cc>
2022-06-28 15:06:16 +0800

    ISCN SQL Schema design

---
+++ sql/iscn.sql
@@ -0,0 +1,37 @@
+CREATE TEMP TABLE IF NOT EXISTS iscn (
+	id BIGSERIAL PRIMARY KEY,
+	iscn_id VARCHAR(80),
+	owner VARCHAR(50),
+	keywords VARCHAR(64)[],
+	fingerprints VARCHAR(256)[],
+	stakeholders JSONB,
+	data JSONB
+);
+
+CREATE TEMP TABLE IF NOT EXISTS meta (
+    id VARCHAR(10) PRIMARY KEY,
+    height BIGINT
+);
+
+INSERT INTO iscn (iscn_id, owner, keywords, fingerprints, stakeholders, data) VALUES
+(
+    'iscn://likecoin-chain/NpoNU1609xrsPS4gxFsALLYoEff4b0JMQSxN-MuI3VE/1',
+    'like18lyaxdl7x6fc6slp77248vvuej936779jpmxa2',
+    ARRAY['test', 'LikeCoin'],
+    ARRAY['https://depub.blog', 'hash://sha256/bc7c83785cc93fc79b5b4a0a24420df6e837e53f0d294929f19fde5a3abda028'],
+	'[{"id": "cosmos18lyaxdl7x6fc6slp77248vvuej936779pa8y73", "name": "Justin Lin"},
+		{"id": "depub.space", "name": "depub.space"}]',
+    '{"contentMetadata": {"name": "hello"}}'
+);
+
+SELECT * FROM iscn;
+
+INSERT INTO meta VALUES
+    ('iscn', 257374),
+    ('txs', 7773482);
+
+UPDATE meta
+    SET height = 300000
+    WHERE id = 'iscn';
+
+SELECT * FROM meta;
