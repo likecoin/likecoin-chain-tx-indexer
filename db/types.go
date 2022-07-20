605a9f81c6d68550895311044a6b31845eed433c
Justin Lin <wancat@wancat.cc>
2022-07-20 17:33:10 +0800

    type one field per line

--- db/types.go
+++ db/types.go
@@ -9,17 +9,19 @@ import (
 )
 
 type ISCN struct {
-	Iscn                   string
-	IscnPrefix             string
-	Version                int
-	Owner                  string
-	Timestamp              time.Time
-	Ipld                   string
-	Name, Description, Url string
-	Keywords               []string
-	Fingerprints           []string
-	Stakeholders           []byte
-	Data                   []byte
+	Iscn         string
+	IscnPrefix   string
+	Version      int
+	Owner        string
+	Timestamp    time.Time
+	Ipld         string
+	Name         string
+	Description  string
+	Url          string
+	Keywords     []string
+	Fingerprints []string
+	Stakeholders []byte
+	Data         []byte
 }
 
 type NftClass struct {
