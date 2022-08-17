c361a6a3fc8edbbfe351debcc077f19d9d0505ec
Justin Lin <wancat@wancat.cc>
2022-08-17 09:31:46 +0800

    :zap: Update totalvolume sql to nft event based

--- sql/totalvolume.sql
+++ sql/totalvolume.sql
@@ -1,4 +1,8 @@
-CREATE INDEX IF NOT EXISTS idx_first_message ON txs USING GIN ((tx #> '{"tx", "body", "messages", 0}') jsonb_path_ops);
-select sum((tx #>> '{"tx", "body", "messages", 0, "msgs", 0, "amount", 0, "amount"}')::bigint)
-    from txs
-WHERE tx #> '{"tx", "body", "messages", 0}' @> '{"@type": "/cosmos.authz.v1beta1.MsgExec","grantee": "like17m4vwrnhjmd20uu7tst7nv0kap6ee7js69jfrs"}' 
+SELECT sum((tx #>> '{"tx", "body", "messages", 0, "msgs", 0, "amount", 0, "amount"}')::bigint)
+FROM txs
+JOIN (
+    SELECT DISTINCT tx_hash FROM nft_event
+    WHERE sender = 'like17m4vwrnhjmd20uu7tst7nv0kap6ee7js69jfrs'
+        AND action = '/cosmos.nft.v1beta1.MsgSend'
+) t
+ON tx_hash = tx ->> 'txhash'::text
