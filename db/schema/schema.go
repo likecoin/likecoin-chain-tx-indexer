1af56be186c13357a9e32abf745478d6960a06f1
Chung Wu <imwmbm@gmail.com>
2022-11-02 23:06:22 +0800

    ♻️ Refactor schema and migration code

--- db/schema/schema.go
+++ db/schema/schema.go
@@ -3,7 +3,6 @@ package schema
 import (
 	"embed"
 	"fmt"
-	"io"
 	"strconv"
 	"strings"
 
@@ -11,25 +10,10 @@ import (
 )
 
 //go:embed v*.sql
-var fs embed.FS
-
-func readEmbededFile(filename string) (string, error) {
-	f, err := fs.Open(filename)
-	if err != nil {
-		logger.L.Errorw("Error when opening SQL file", "file", filename, "err", err)
-		return "", err
-	}
-	defer f.Close()
-	bz, err := io.ReadAll(f)
-	if err != nil {
-		logger.L.Errorw("Error when reading SQL file", "file", filename, "err", err)
-		return "", err
-	}
-	return string(bz), nil
-}
+var embedFS embed.FS
 
 func GetVersionSQLMap() (versionSqlMap map[uint64]string, codeSchemaVersion uint64, err error) {
-	files, err := fs.ReadDir(".")
+	files, err := embedFS.ReadDir(".")
 	if err != nil {
 		return nil, 0, err
 	}
@@ -41,28 +25,28 @@ func GetVersionSQLMap() (versionSqlMap map[uint64]string, codeSchemaVersion uint
 		numericPart := path[len(path)-1][1 : len(filename)-4]
 		version, err := strconv.ParseUint(numericPart, 10, 64)
 		if err != nil {
-			logger.L.Errorw("Invalid SQL filename, expect v[schema_version].sql", "filename", filename)
+			logger.L.Errorw("invalid SQL filename, expect v[schema_version].sql", "filename", filename)
 			return nil, 0, err
 		}
 		if version == 0 {
-			errMsg := "Invalid SQL schema version: 0"
+			errMsg := "invalid SQL schema version: 0"
 			logger.L.Errorw(errMsg)
 			return nil, 0, fmt.Errorf(errMsg)
 		}
 		if version > codeSchemaVersion {
 			codeSchemaVersion = version
 		}
-		sql, err := readEmbededFile(filename)
+		sqlBz, err := embedFS.ReadFile(filename)
 		if err != nil {
 			return nil, 0, err
 		}
-		versionSqlMap[uint64(version)] = sql
+		versionSqlMap[uint64(version)] = string(sqlBz)
 	}
 	// always check existence of every version so it would be robust and won't "partially work"
 	for version := uint64(1); version <= codeSchemaVersion; version++ {
 		if _, ok := versionSqlMap[version]; !ok {
-			logger.L.Errorw("Missing SQL file for schema version", "version", version)
-			return nil, 0, fmt.Errorf("Missing SQL file for schema version %d", version)
+			logger.L.Errorw("missing SQL file for schema version", "version", version)
+			return nil, 0, fmt.Errorf("missing SQL file for schema version %d", version)
 		}
 	}
 	return versionSqlMap, codeSchemaVersion, nil
