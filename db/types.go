cd29a87182d87dd39618373c5c90fd14f7c81f46
William Chong <me@williamchong.cloud>
2022-11-11 17:09:26 +0800

    ✨ Add query class by iscn owner

--- db/types.go
+++ db/types.go
@@ -183,6 +183,7 @@ type iscnResponseData struct {
 type QueryClassRequest struct {
 	IscnIdPrefix string `form:"iscn_id_prefix"`
 	Account      string `form:"account"`
+	IscnOwner    string `form:"iscn_owner"`
 	Expand       bool   `form:"expand"`
 }
 
