165bf037d123ddb8d6711e2a6c8721cdb789994f
Chung Wu <imwmbm@gmail.com>
2023-01-26 15:41:15 +0800

    🔥 Remove useless extractor test

--- extractor/extractor_test.go
+++ extractor/extractor_test.go
@@ -3,19 +3,8 @@ package extractor_test
 import (
 	"testing"
 
-	"github.com/likecoin/likecoin-chain-tx-indexer/db"
-	"github.com/likecoin/likecoin-chain-tx-indexer/extractor"
 	. "github.com/likecoin/likecoin-chain-tx-indexer/test"
 )
-
-func TestExtract(t *testing.T) {
-	for i := 0; i < 2; i++ {
-		_, err := db.Extract(Conn, extractor.ExtractFunc)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-}
 
 func TestMain(m *testing.M) {
 	SetupDbAndRunTest(m, nil)
